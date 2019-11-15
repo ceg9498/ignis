6c45e72a1100122c809f6de20116a2064540413b
C.E. Gagne <ceg9498@gmail.com>
2019-11-14 19:27:23 -0500

    Add groceryItem type

---
+++ src/types/groceryItem.ts
@@ -0,0 +1,20 @@
+import { ingredient } from './ingredient';
+import { quantity } from './quantity';
+
+export type groceryItem = {
+  quantity: quantity,
+  name: string,
+  isDone: boolean
+};
+
+export class GroceryItem implements groceryItem {
+  quantity: quantity;
+  name: string;
+  isDone: boolean;
+
+  constructor(ingredient: ingredient, isDone: boolean) {
+    this.quantity = ingredient.quantity;
+    this.name = ingredient.name;
+    this.isDone = isDone;
+  }
+}
